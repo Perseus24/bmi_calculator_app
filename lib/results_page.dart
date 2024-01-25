914503446169f773f25d6494142c2a992e219114
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-01-25 14:50:59 +0800

    feat(project): Added a file for constants and for the results page

---
+++ lib/results_page.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Text('Imong mama'),
+    );
+  }
+}
