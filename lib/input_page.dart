e150eb221d5675e6740c4a1668b426d59eb2c9e8
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-01-25 11:36:11 +0800

    feat(project): Created new class for the containers for the homepage

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,21 +1,72 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const botomContainerColor = Color(0xFFEB1555);
+const homeWidgetsColor = Color(0xFF1D1E33);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                HomePageWidgets(changeColor: homeWidgetsColor,),
+                HomePageWidgets(changeColor: homeWidgetsColor,)
+              ],
+            ),
+          ),
+          HomePageWidgets(changeColor: homeWidgetsColor,),
+          Expanded(
+            child: Row(
+              children: [
+                HomePageWidgets(changeColor: homeWidgetsColor,),
+                HomePageWidgets(changeColor: homeWidgetsColor,)
+              ],
+            ),
+          ),
+          Container(
+            color: botomContainerColor,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
+      )
+    );
+  }
+}
+
+class HomePageWidgets extends StatelessWidget {
+
+  HomePageWidgets({required this.changeColor});
+  final Color changeColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: changeColor,
+            borderRadius: BorderRadius.circular(10)
+        ),
       ),
     );
   }
 }
+
+//Color(0xFF1D1E33),
