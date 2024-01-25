68e14413b219a6626cc6513691b945085f1492f0
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-01-25 10:21:23 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Color(0xFF0A0E21),
+          titleTextStyle: TextStyle(color: Colors.white)
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.white)
+        )
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
